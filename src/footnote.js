7c3ae78ebf584fc92a39c1ea6579c41aeb0df8a0
alisx <finddly@163.com>
2023-05-23 14:39:14 +0800

    fixbug: 如果页面中没有外链，就不再生成 参考资料

--- src/footnote.js
+++ src/footnote.js
@@ -29,37 +29,48 @@ function replaceLink(num, link) {
 // 微信内链判断正则表达式
 const wxLinkRegex = /^https?:\/\/(?:www\.)?(?:(?:mp\.weixin|pay\.weixin)\.qq\.com|weixin\.qq\.com\/).*$/i;
 
-// 创建脚注
-function CreateFootnote() {
+function createFootnoteSection() {
     var h3 = document.createElement('h3');
     h3.setAttribute("class", "footnotes-sep");
     h3.innerText = "参考资料";
-
-    var section = document.createElement('section');
-    // 你的代码
+    return {
+        section: document.createElement('section'),
+        title: h3
+    }
+}
+// 创建脚注
+function CreateFootnote() {
     const links = document.querySelectorAll("a");
-    var linknum = 1;
-    var linkmap = {};
-    links.forEach(link => {
-        if (!wxLinkRegex.test(link.href)) { // 检查是否微信链接
-            // 对链接进行排重，如果已经有了就沿用之前的序号
-            var num = linkmap[link.href];
-            if (num == null) {
-                // 生成元素 如果没有 title 属性就用 text 代替
-                section.appendChild(CreateFootnoteItem(linknum, (link.title || link.text), link.attributes.href.value));
-                linkmap[link.href] = linknum;
-                num = linknum;
-                console.log(num);
-                linknum += 1;
-                console.log(num);
+    if (links.length > 0) {
+        var linknum = 1;
+        var linkmap = {};
+        var section = null;
+        links.forEach(link => {
+            if (!wxLinkRegex.test(link.href)) { // 检查是否微信链接
+                // 对链接进行排重，如果已经有了就沿用之前的序号
+                var num = linkmap[link.href];
+                if (num == null) {
+                    // 生成元素 如果没有 title 属性就用 text 代替
+                    section = section || document.createElement('section');
+                    section.appendChild(CreateFootnoteItem(linknum, (link.title || link.text), link.attributes.href.value));
+                    linkmap[link.href] = linknum;
+                    num = linknum;
+                    console.log(num);
+                    linknum += 1;
+                    console.log(num);
+                }
+                replaceLink(num, link);// 替换链接
             }
-            replaceLink(num, link);// 替换链接
+        });
+        if (section) {
+            var h3 = document.createElement('h3');
+            h3.setAttribute("class", "footnotes-sep");
+            h3.innerText = "参考资料";
+            // 插入脚注元素 请查看 Html 文件中，文章内容块的标记，将 #write 换成目标内容块的标记
+            document.querySelector('#write').appendChild(h3)
+            document.querySelector('#write').appendChild(section);
         }
-    });
-
-    // 插入脚注元素 请查看 Html 文件中，文章内容块的标记，将 #write 换成目标内容块的标记
-    document.querySelector('#write').appendChild(h3)
-    document.querySelector('#write').appendChild(section);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
