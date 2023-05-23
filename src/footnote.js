54926297beffb825b0b125f3f00effe375937f15
alisx <finddly@163.com>
2023-05-23 17:18:56 +0800

    取消自动执行

--- src/footnote.js
+++ src/footnote.js
@@ -73,6 +73,6 @@ function CreateFootnote() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    CreateFootnote();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     CreateFootnote();
+// });
